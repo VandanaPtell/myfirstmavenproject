efa75d4c6224482efd77df5eae99d10ee0f30851
VandanaPtell <vandanapatel@outlookemail.ca>
2024-03-10 11:55:51 -0400

    calculator program with unit testcase

---
+++ src/test/java/mavenpracticeproject/Calculatortest.java
@@ -0,0 +1,29 @@
+package mavenpracticeproject
+;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Calculatortest {
+@Test
+public void testadd(){
+assertEquals(10,Calculator.add(5,5));
+assertEquals(-5,Calculator.add(-7,2));	
+}
+@Test
+public void testsubstract() {
+assertEquals(5,Calculator.substract(7,2));
+assertEquals(-2,Calculator.substract(-4,-2));
+}
+@Test
+public void testmultiply() {
+	assertEquals(50,Calculator.multiply(10,5));
+	assertEquals(-6,Calculator.multiply(2,-3));
+}
+@Test
+public void divide() {
+	assertEquals(2,Calculator.divide(10,5));
+	assertEquals(-3,Calculator.divide(-9,3));
+}
+}
